d834d2e8219bd680209d40b42035d273d0f3f51c
Nihed ATTIA <nihed.attia@esprit.tn>
2023-10-12 20:18:25 +0200

    add Log4j

--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.kaddem.controllers;
 
+
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
@@ -2,6 +2,8 @@ package tn.esprit.spring.kaddem.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,6 +23,8 @@ import java.util.Set;
 @Service
 @Slf4j
 public class EtudiantServiceImpl implements IEtudiantService{
+
+	private static final Logger logger = LoggerFactory.getLogger(EtudiantServiceImpl.class);
 	@Autowired
 	EtudiantRepository etudiantRepository ;
 	@Autowired
@@ -30,42 +34,67 @@ public class EtudiantServiceImpl implements IEtudiantService{
     @Autowired
     DepartementRepository departementRepository;
 	public List<Etudiant> retrieveAllEtudiants(){
-	return (List<Etudiant>) etudiantRepository.findAll();
+		logger.info("Retrieving all Etudiants");
+		return (List<Etudiant>) etudiantRepository.findAll();
 	}
 
 	public Etudiant addEtudiant (Etudiant e){
+		logger.info("Adding Etudiant: {}", e);
 		return etudiantRepository.save(e);
 	}
 
 	public Etudiant updateEtudiant (Etudiant e){
+		logger.info("Updating Etudiant: {}", e);
 		return etudiantRepository.save(e);
 	}
 
 	public Etudiant retrieveEtudiant(Integer  idEtudiant){
+
+		logger.info("Updating Etudiant: {}",idEtudiant);
 		return etudiantRepository.findById(idEtudiant).get();
 	}
 
 	public void removeEtudiant(Integer idEtudiant){
+
+	logger.info("removing Etudiant:{} ",idEtudiant );
 	Etudiant e=retrieveEtudiant(idEtudiant);
 	etudiantRepository.delete(e);
 	}
 
 	public void assignEtudiantToDepartement (Integer etudiantId, Integer departementId){
-        Etudiant etudiant = etudiantRepository.findById(etudiantId).orElse(null);
-        Departement departement = departementRepository.findById(departementId).orElse(null);
-        etudiant.setDepartement(departement);
-        etudiantRepository.save(etudiant);
+
+		logger.info("Assigning Etudiant (ID={}) to Departement (ID={})", etudiantId, departementId);
+
+		Etudiant etudiant = etudiantRepository.findById(etudiantId).orElse(null);
+		Departement departement = departementRepository.findById(departementId).orElse(null);
+
+		if (etudiant != null && departement != null) {
+			etudiant.setDepartement(departement);
+			etudiantRepository.save(etudiant);
+			logger.info("Etudiant (ID={}) assigned to Departement (ID={}) successfully", etudiantId, departementId);
+		} else {
+			logger.warn("Assignment failed. Etudiant (ID={}) or Departement (ID={}) not found.", etudiantId, departementId);
+		}
+
 	}
 	@Transactional
 	public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe){
+		logger.info("Adding and assigning Etudiant to Equipe and Contract: {}", e);
 		Contrat c=contratRepository.findById(idContrat).orElse(null);
 		Equipe eq=equipeRepository.findById(idEquipe).orElse(null);
 		c.setEtudiant(e);
 		eq.getEtudiants().add(e);
+		logger.info("Etudiant added and assigned successfully");
 return e;
 	}
 
 	public 	List<Etudiant> getEtudiantsByDepartement (Integer idDepartement){
-return  etudiantRepository.findEtudiantsByDepartement_IdDepart((idDepartement));
+
+		logger.info("Fetching Etudiants by Departement: ID={}", idDepartement);
+
+		List<Etudiant> etudiants = etudiantRepository.findEtudiantsByDepartement_IdDepart(idDepartement);
+
+		logger.info("Fetched {} Etudiants by Departement", etudiants.size());
+		return etudiants;
 	}
 }
